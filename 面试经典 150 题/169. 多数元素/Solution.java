ec9cc2965dfc11a15c16a4337176bb9e03950fa3
Zaixuele <836967865@qq.com>
2024-09-17 21:53:02 +0800

    day03

---
+++ 面试经典 150 题/169. 多数元素/Solution.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        // 时间复杂度O(Nlogn),没有满足进阶要求：时间复杂度为O(n)
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
