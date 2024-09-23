125fab4510228b2aa40d7f1ca316c3dd493a3fca
Zaixuele <836967865@qq.com>
2024-09-23 15:03:30 +0800

    day09

---
+++ LeetCode 75/334. 递增的三元子序列/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int head = 0, end = nums.length - 1;
+
+        while (head < end) {
+            if (nums[head] > nums[end]) {
+                
+            }
+            
+        }
+
+    }
+}
